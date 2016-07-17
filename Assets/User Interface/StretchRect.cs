d68b1713bfcbce273f77a4be660ecaee3fd7d0de
Justas <glodjus@gmail.com>
2016-07-17 21:44:43 +0300

    Screen UI images added.

---
+++ Assets/User Interface/StretchRect.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class StretchRect : MonoBehaviour {
+
+    public float originalWidth;
+    public float originalHeight;
+    private RectTransform transRect;
+
+	void Start () {
+        transRect = GetComponent<RectTransform>();
+        SetSize();
+    }
+
+	void Update () {
+#if UNITY_EDITOR
+        SetSize();
+#endif
+    }
+
+    private void SetSize()
+    {
+        if (transRect)
+        {
+            transRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Screen.width * originalHeight / originalWidth);
+        }
+
+    }
+}
