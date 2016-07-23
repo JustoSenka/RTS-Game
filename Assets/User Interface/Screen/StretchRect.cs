fe86e70936edab00ee4c5566c1bc77d3eeeb5a8f
Justas <glodjus@gmail.com>
2016-07-23 19:25:36 +0300

    UI Stretching to multiple resolutions done.

--- Assets/User Interface/Screen/StretchRect.cs
+++ Assets/User Interface/Screen/StretchRect.cs
@@ -9,7 +9,17 @@ public class StretchRect : MonoBehaviour {
 
     public float originalWidth;
     public float originalHeight;
+
+    public StretchType stretchType = StretchType.Width;
+    public bool keepWidthHeightRatio = true;
+    public bool fitToScreen = true;
+
+    public float preferredHeightPercentage;
+    public float preferredWidthPercentage;
+
     private RectTransform transRect;
+    private float width;
+    private float height;
 
 	void Start () {
         transRect = GetComponent<RectTransform>();
@@ -26,8 +36,56 @@ public class StretchRect : MonoBehaviour {
     {
         if (transRect)
         {
-            transRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Screen.width * originalHeight / originalWidth);
-        }
+            SetSizeAccordingToStretchType();
 
+            if (fitToScreen)
+            {
+                SetSizeToFitScreen();
+            }
+
+            if (keepWidthHeightRatio)
+            {
+                transRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+                transRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+            }
+            else if (stretchType.Equals(StretchType.Width))
+            {
+                transRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+            }
+            else
+            {
+                transRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+            }
+        }
+    }
+
+    private void SetSizeAccordingToStretchType()
+    {
+        if (stretchType.Equals(StretchType.Width))
+        {
+            width = Screen.width * preferredWidthPercentage;
+            height = originalHeight * (width / originalWidth);
+        }
+        if (stretchType.Equals(StretchType.Height))
+        {
+            height = Screen.height * preferredHeightPercentage;
+            width = originalWidth * (height / originalHeight);
+        }
+    }
+
+    private void SetSizeToFitScreen()
+    {
+        if (height > Screen.height)
+        {
+            height = Screen.height;
+            width = originalWidth * (height / originalHeight);
+        }
+        if (width > Screen.width)
+        {
+            width = Screen.width;
+            height = originalHeight * (width / originalWidth);
+        }
     }
 }
+
+public enum StretchType { Height, Width }
