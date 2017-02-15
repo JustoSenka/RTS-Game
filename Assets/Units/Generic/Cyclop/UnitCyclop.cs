29575aef0c9e422c2e0996aeae16482f54ec1446
Justas <glodjus@gmail.com>
2017-02-15 03:57:18 +0200

    Adding bunch of new Models + Animations.
    Now works with humanoid, sample scene added.

---
+++ Assets/Units/Generic/Cyclop/UnitCyclop.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnitCyclop : UnitHero
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    public void UpdateRandomAttack()
+    {
+        animator.SetFloat("RandomAttack", (animator.GetFloat("RandomAttack") + 1) % 2);
+    }
+
+    public override void PerformCommand(Command command, bool resetPendingCommand = true)
+    {
+        if (isDead)
+            return;
+
+        base.PerformCommand(command, resetPendingCommand);
+    }
+}
---
+++ Assets/Units/Generic/Skeleton/UnitSkeleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnitSkeleton : UnitHero
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    public override void PerformCommand(Command command, bool resetPendingCommand = true)
+    {
+        if (isDead)
+            return;
+
+        base.PerformCommand(command, resetPendingCommand);
+    }
+}
